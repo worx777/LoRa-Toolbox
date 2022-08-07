a2fb4a18342c06addb2b26ceb238e83f78170933
Manuel <51753744+worx777@users.noreply.github.com>
2022-08-07 15:44:06 +0200

    Public Release

---
+++ LoPy/boot.py
@@ -0,0 +1,20 @@
+##
+## Project: LoRa Toolbox
+## File name:: boot.py
+## Created by: Manuel Bechel (SES) for Trier University of Applied Sciences
+## Version: 1.0
+## Date: 30.03.2022
+##
+## Description: The LoRa Toolbox provides a simple start to play around with
+## LoRa enbaled PyCom LoPy 4 Microcontrollers. Targeted audience for this project
+## are students, teachers and other interested persons with no or only basic 
+## knowledge in radio frequency based transmissions and reception or even 
+## coding at all. The more advanced audience can adapt, improve or extend this code.
+## 
+##
+
+#  boot.py 
+# This code will run once during the boot
+# Wifi settings were more stable when brought up during the main.py file
+# This may change in future updates of the boards!
+print('Boot.py')
